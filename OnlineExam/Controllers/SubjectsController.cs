089d5e8fd8228d6ebc05a1f44d569e1e2f219cf7
Ashwin6899 <imashwin.c6@gmail.com>
2021-09-29 12:49:42 +0530

    Subjects

---
+++ OnlineExam/Controllers/SubjectsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineExam.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineExam.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubjectsController : ControllerBase
+    {
+        OnlineExamContext db;
+        public SubjectsController(OnlineExamContext context)
+        {
+            db = context;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(db.Subjects);
+        }
+
+    }
+}
