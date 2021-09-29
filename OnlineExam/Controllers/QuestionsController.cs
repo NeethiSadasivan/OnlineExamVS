f5754fc02752283cdd0ecc75d97d26c39f69e37b
NeethiSadasivan <neethinandu1@gmail.com>
2021-09-29 12:27:46 +0530

    methods for all the levels

--- OnlineExam/Controllers/QuestionsController.cs
+++ OnlineExam/Controllers/QuestionsController.cs
@@ -17,12 +17,12 @@ namespace OnlineExam.Controllers
         {
             db = context;
         }
-        [HttpGet]
-        public IActionResult GetForl1(string sub,int level)
+        [HttpGet("Level1")]
+        public IActionResult GetForl1(string sub)
         {
             var qns = (from q in db.Questions
                      where q.Subject.Subjectname == sub
-                     where q.Level==level
+                     where q.Level==1
                      join s in db.Subjects on q.Subjectid equals s.Subjectid
 
                      select new
@@ -33,6 +33,42 @@ namespace OnlineExam.Controllers
                          q.Option3,
                          q.Option4
                      }).ToList();
+            return Ok(qns);
+        }
+        [HttpGet("Level2")]
+        public IActionResult GetForl2(string sub)
+        {
+            var qns = (from q in db.Questions
+                       where q.Subject.Subjectname == sub
+                       where q.Level == 2
+                       join s in db.Subjects on q.Subjectid equals s.Subjectid
+
+                       select new
+                       {
+                           q.Question,
+                           q.Option1,
+                           q.Option2,
+                           q.Option3,
+                           q.Option4
+                       }).ToList();
+            return Ok(qns);
+        }
+        [HttpGet("Level3")]
+        public IActionResult GetForl3(string sub)
+        {
+            var qns = (from q in db.Questions
+                       where q.Subject.Subjectname == sub
+                       where q.Level == 3
+                       join s in db.Subjects on q.Subjectid equals s.Subjectid
+
+                       select new
+                       {
+                           q.Question,
+                           q.Option1,
+                           q.Option2,
+                           q.Option3,
+                           q.Option4
+                       }).ToList();
             return Ok(qns);
         }
     }
