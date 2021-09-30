eed0257c04ebe58b77d3fbf650e56d0bcd300ec9
kgoel99 <kunalgoel25061999@gmail.com>
2021-09-30 21:35:16 +0530

    Added Add question controller

--- OnlineExam/Models/OnlineExamContext.cs
+++ OnlineExam/Models/OnlineExamContext.cs
@@ -31,7 +31,7 @@ namespace OnlineExam.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=NEETHI-PC\\SQLEXPRESS;Database=OnlineExam;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=DESKTOP-KRM3QTE;Database=OnlineExam;Trusted_Connection=True;");
             }
         }
 
