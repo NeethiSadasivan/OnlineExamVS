ea0099ab90b3c63bac6dff6585a79a96fae1d6ef
Ashwin6899 <imashwin.c6@gmail.com>
2021-09-30 15:11:53 +0530

    Updated users

---
+++ OnlineExam/Controllers/StudentDetailController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OnlineExam.Models;
+
+namespace OnlineExam.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentDetailController : ControllerBase
+    {
+        OnlineExamContext db;
+
+        public StudentDetailController(OnlineExamContext context)
+        {
+            db = context;
+        }
+
+        [HttpGet]
+        public IActionResult getStudentResultsByState(int? Subjectid, string State)
+        {
+            //Result r = new Result();
+            var p = (from u in db.Users
+                     join r in db.Result on u.Userid equals r.Userid
+                     where r.Subjectid == Subjectid && u.State == State
+                     select new
+                     {
+                         u.Userid,
+                         u.Username,
+                         u.Email,
+                         u.Mobile,
+                         u.Dob,
+                         u.City,
+                         u.State,
+                         r.Subjectid,
+                         r.Level1marks,
+                         r.Level2marks,
+                         r.Level3marks
+                     }).ToList();
+            return Ok(p);
+        }
+    }
+}
